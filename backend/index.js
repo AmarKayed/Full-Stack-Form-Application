59bfc7bc348286dcf1ebb6646c7a32f0b5bac2f5
Amar Kayed <amar.kayed18@gmail.com>
2022-02-07 22:24:55 +0200

    Fixed HTTP Request Errors

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 
 
+const cors = require('cors');
+app.use(cors());        // This fixes the error from the Axios request to the backend
+app.use(express.json()); // This fixes the error of parsing the body from the frontend to the backend as a json object
+
+
 app.post('/create', (req, res) => {
+  // console.log(req.body);
   console.log(req.body.name);
   res.send(req.body.name);
 })

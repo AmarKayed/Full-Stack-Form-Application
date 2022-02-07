4a3f67061772405996e1f1f9c67e97029588d217
Amar Kayed <amar.kayed18@gmail.com>
2022-02-07 21:51:41 +0200

    Added useState Hooks for all forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,46 @@
 import React from "react";
 import './App.css'
 // import Form from './components/Form/Form.js'
+import { useState } from "react";
 
 const App = () => {
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState('');
+  const [position, setPosition] = useState('');
+  const [wage, setWage] = useState(0);
+
+
+  const displayInfo = () => {
+    console.log(name, age, country, position, wage);
+  }
   return (
     <div className="App">
       <div className = "formArea">
         <form>
           <label>Name:</label>
-          <input type = "text"></input>
+          <input type = "text" onChange={(event) => {setName(event.target.value); }}></input>
         </form>
       
         <form>
           <label>Age:</label>
-          <input type = "number"></input>
+          <input type = "number" onChange={(event) => {setAge(event.target.value)}}></input>
         </form>
 
         <form>
           <label>Country:</label>
-          <input type = "text"></input>
+          <input type = "text" onChange={(event) => {setCountry(event.target.value)}}></input>
         </form>
 
         <form>
           <label>Position:</label>
-          <input type = "text"></input>
+          <input type = "text" onChange={(event) => {setPosition(event.target.value)}}></input>
         </form>
 
         <form>
           <label>Wage(year):</label>
-          <input type = "number"></input>
+          <input type = "number" onChange={(event) => {setWage(event.target.value)}}></input>
         </form>
 
         {/* 
@@ -41,7 +53,7 @@ const App = () => {
 
       </div>
 
-      <button className = "submitButton">Add Employee</button>
+      <button className = "submitButton" onClick={displayInfo}>Add Employee</button>
       
     </div>
   );
